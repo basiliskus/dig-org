65516a60b79bea5eaea689dc6de40ca16020aaf4
basiliskus <basilio.bogado@gmail.com>
2020-05-29 11:28:51 -0700

    add argument parser module

---
+++ modules/aparser.py
@@ -0,0 +1,17 @@
+import argparse
+
+# arguments = [{
+#   'name': '',
+#   'action': '',
+#   'help': ''
+# }]
+
+def get_args(description='', arguments=[]):
+  parser = argparse.ArgumentParser(description=description)
+  for argument in arguments:
+    parser.add_argument(
+      arguments['name'],
+      action=arguments['action'],
+      help=arguments['help']
+    )
+  return parser.parse_args()
