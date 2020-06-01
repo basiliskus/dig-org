0d2748b2e776d2ccbde351ebff2302c76f2f2828
basiliskus <basilio.bogado@gmail.com>
2020-05-31 19:11:49 -0700

    add function name to file log

--- modules/log.py
+++ modules/log.py
@@ -10,7 +10,7 @@ def get_logger(fname, log_path='logs', level=logging.DEBUG):
   logger.addHandler(sh)
 
   datefmt = '%Y/%m/%d %H:%M:%S'
-  fhformat = '[%(asctime)s][%(levelname)s] %(message)s'
+  fhformat = '[%(asctime)s][%(levelname)s][%(funcName)s] %(message)s'
   log_fpath = f'{log_path}/{fname}{LOG_EXTENSION}'
   fh = logging.FileHandler(log_fpath)
   fh.setFormatter(logging.Formatter(fhformat, datefmt))
