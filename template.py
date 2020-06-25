ab8c3bcdf6ad8202ead9b743c3ac1762a1d7bad7
basiliskus <basilio.bogado@gmail.com>
2020-06-24 17:06:55 -0700

    update template to add arg parser, try/except and config file consolidation

--- template.py
+++ template.py
@@ -1,12 +1,13 @@
+import argparse
 from pathlib import Path
 
 from modules import log
 from modules import utils
 from modules import config
-from modules import aparser
 
-global_config = config.get_config('global')
-log_path = Path(global_config['paths']['log_path'])
+
+config = config.get_config('config')
+log_path = Path(config['global']['log_path'])
 
 script_name = utils.get_script_name(__file__)
 logger = log.get_logger(script_name, log_path=log_path)
@@ -14,6 +15,25 @@ logger = log.get_logger(script_name, log_path=log_path)
 def main(args):
   print('this is a template')
 
+
+def get_parser():
+  parser = argparse.ArgumentParser(
+    description='Template script description',
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    )
+  parser.add_argument(
+    '-t',
+    '--test',
+    action='store_true',
+    help = 'Test run. No files will be modified'
+    )
+  return parser
+
+
 if __name__ == "__main__":
-  args = aparser.get_args()
-  main(args)
+  parser = get_parser()
+  args = vars(parser.parse_args())
+  try:
+    main(args)
+  except Exception as e:
+    logger.exception(e)
