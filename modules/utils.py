07f81c0c11decfffc0b4ff096218cdd69dfb85ef
basiliskus <basilio.bogado@gmail.com>
2020-06-29 15:23:49 -0700

    fix parameter

--- modules/utils.py
+++ modules/utils.py
@@ -3,7 +3,7 @@ import os
 def get_script_name(fname):
   return os.path.splitext(os.path.basename(fname))[0]
 
-def strip(self, string, rmv):
+def strip(string, rmv):
   for r in rmv:
     string = string.replace(r, '')
   return string
